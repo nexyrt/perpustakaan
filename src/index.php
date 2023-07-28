f80a21515bd7a5834c9a58ee14848663b94430c6
Rizky Hamdani <rizkyhamdani67@gmail.com>
2023-07-29 02:39:35 +0800

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Sistem Informasi Manajemen Perpustakaan</title>
+    <link href="../styles/output.css" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <h1 class="bg-red-600">
+            Hello world!
+            Hello there sir, i'm new in this web development world and i need your guide for me to survive in this position.
+            I'm also aiming for the Android Developer Position. Which i am sure that i'm capable of doing it tasks and i also already
+            had some of it's basic knowledge such as using Android Studio, i learn kotlin like how to write the code and the syntax
+        </h1>
+    </main>
+</body>
+
+</html>
