87bf5aa68bdddd2952a6ca02e49ad35fe8c4d49d
Rizky Hamdani <rizkyhamdani67@gmail.com>
2023-08-01 03:39:25 +0800

    Penambahan Icon Website

--- src/index.php
+++ src/index.php
@@ -9,14 +9,15 @@
 </head>
 
 <body>
+    <header>
+
+    </header>
     <main>
-        <h1 class="bg-red-600">
-            Hello world!
-            Hello there sir, i'm new in this web development world and i need your guide for me to survive in this position.
-            I'm also aiming for the Android Developer Position. Which i am sure that i'm capable of doing it tasks and i also already
-            had some of it's basic knowledge such as using Android Studio, i learn kotlin like how to write the code and the syntax
-        </h1>
+        
     </main>
+    <footer>
+
+    </footer>
 </body>
 
 </html>
